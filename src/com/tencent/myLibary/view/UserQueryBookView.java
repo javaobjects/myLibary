da9fbc750e4ac87abd76afc74dd84fd16cd71e38
yanbo0039 <yanbo0039@yeah.net>
2019-08-20 15:35:54 +0800

    嵌入式窗体-用户查询图书窗体的制作

--- src/com/tencent/myLibary/view/UserQueryBookView.java
+++ src/com/tencent/myLibary/view/UserQueryBookView.java
@@ -1,6 +1,18 @@
 package com.tencent.myLibary.view;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import com.tencent.myLibary.entity.User;
 
@@ -18,12 +30,31 @@ public class UserQueryBookView extends JInternalFrame {
 	
 	private User user;
 
-
+	/** 窗体中的最外层的面板 */
+	private JPanel panel_common;
+	/** 左面板 */
+	private JScrollPane panel_left;
+	/** 右面板 */
+	private JPanel panel_right;
+	/** 存放数据的表格控件 */
+	private JTable table;
+	/** 查询类型标签 */
+	private JLabel lb_query_type;
+	/** 查询类型下拉框 */
+	private JComboBox<String> cb_query_type;
+	/** 查询按钮 */
+	private JButton btn_query;
+	/** 借书按钮 */
+	private JButton btn_lend;
+	/** 退出按钮 */
+	private JButton btn_exit;
+	
+	
 	/** 构造方法 */
 	public UserQueryBookView(User user) {
 		this.user = user;
-//		init();
-//		registerListener();
+		init();
+		registerListener();
 		this.setTitle("用户查询图书窗体");
 		this.setSize(600, 500);
 		// 设置窗体可以关闭
@@ -35,5 +66,62 @@ public class UserQueryBookView extends JInternalFrame {
 		this.setVisible(true);
 
 	}
+	/** 初始化组件装配组件的方法 */
+	private void init() {
+		lb_query_type = new JLabel("查询类型：");
+		cb_query_type = new JComboBox<String>(new String[] { "所有图书", "热门图书",
+				"可借图书", "不可借图书" });
+		btn_query = new JButton("查    询");
+		btn_lend = new JButton("借    书");
+		btn_exit = new JButton("退     出");
 
+		table = new JTable();
+		panel_left = new JScrollPane(table);
+
+		panel_right = new JPanel(new GridLayout(7, 1, 0, 20));
+		panel_right.setBorder(BorderFactory.createTitledBorder(
+				BorderFactory.createRaisedBevelBorder(), "查询条件"));
+		panel_right.add(lb_query_type);
+		panel_right.add(cb_query_type);
+		panel_right.add(btn_query);
+		panel_right.add(btn_lend);
+		panel_right.add(new JLabel());
+		panel_right.add(new JLabel());
+		panel_right.add(btn_exit);
+
+		panel_common = new JPanel(new BorderLayout());
+		panel_common.add(panel_left, BorderLayout.CENTER);
+		panel_common.add(panel_right, BorderLayout.EAST);
+
+		this.add(panel_common);
+	}
+	
+	private void registerListener() {
+		btn_query.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("111");
+			}
+		});
+		btn_lend.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("222");
+			}
+		});
+		btn_exit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("333");
+			}
+		});
+		
+	}
+	
+	
+	
+	
 }
